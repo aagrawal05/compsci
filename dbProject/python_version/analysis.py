57cb9325383caa8363db0e7b1bd46c04794be264
Aditya Agrawal <adityaagrawal@Adityas-MacBook-Pro.local>
2023-03-03 21:51:38 +0800

    DB Project V1

--- dbProject/python_version/analysis.py
+++ dbProject/python_version/analysis.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt
+import scipy.cluster
 import pandas as pd
 import sqlite3
 
@@ -17,6 +18,42 @@ df = pd.read_sql_query(query, conn)
 
 #Close the connection
 conn.close()
+
+def cluster(df):
+    print("Clustering...")
+    # If not an empty dataframe
+    if df.empty:
+        return None, None
+
+    # Find a good number of clusters
+    # Use the elbow method
+    # https://en.wikipedia.org/wiki/Elbow_method_(clustering)
+    print("Finding number of clusters...")
+    #1. Create a list of the number of clusters to test
+    num_clusters = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+    #2. Create a list of the SSE for each number of clusters
+    sse = []
+    for num in num_clusters:
+        #3. For each number of clusters, run kmeans and calculate the SSE
+        #4. Add the SSE to the list
+        sse.append(scipy.cluster.vq.kmeans2(df, num)[1].sum())
+    # Find the number of clusters that minimizes the SSE
+    #1. Find the index of the minimum SSE
+    min_index = sse.index(min(sse))
+    #2. Return the number of clusters that corresponds to the minimum SSE
+    k = num_clusters[min_index]
+
+    # Perform k-means clustering on the data
+    # Return the centroids
+    print("Clustering...")
+    centroids, _ = scipy.cluster.vq.kmeans2(df, k)
+    #return a dataframe with all the points and their cluster
+    clustered_df = pd.DataFrame({
+        'latitude': df['latitude'],
+        'longitude': df['longitude'],
+        'cluster': _
+    })
+    return centroids, clustered_df
 
 # Generate a depth map for the data with interpolation for smoothing
 def generate_depth_map(df, resolution):
@@ -37,28 +74,41 @@ def generate_depth_map(df, resolution):
         if x_index < depth_map.shape[0] and y_index < depth_map.shape[1]:
             depth_map[x_index][y_index] += 1
     
-    #2. Interpolate the depth map by averaging the surrounding points
-    print("Interpolating depth map...")
-    for i in range(depth_map.shape[0]):
-        for j in range(depth_map.shape[1]):
-            depth_map[i][j] = numpy.average(depth_map[max(0, i-1):min(depth_map.shape[0], i+2), max(0, j-1):min(depth_map.shape[1], j+2)])
-
     #3. Return the depth map
     return depth_map
+
+def plot_3d(depth_map, ax):
+    # Create a meshgrid for the depth map
+    x_start = df['longitude'].min()
+    y_start = df['latitude'].min()
+    x_end = df['longitude'].max()
+    y_end = df['latitude'].max()
+    x = numpy.linspace(x_start, x_end, depth_map.shape[1])
+    y = numpy.linspace(y_start, y_end, depth_map.shape[0])
+    X, Y = numpy.meshgrid(x, y)
+
+    ax.plot_surface(X, Y, depth_map, cmap='viridis', linewidth=0)
 
 # TEST FUNCTION
 # Render the depth map as an image
 def render_depth_map(depth_map):
-    print("Rendering depth map...")
-    plt.imshow(depth_map, cmap='hot', extent=[0, depth_map.shape[1], depth_map.shape[0], 0], vmin=0.0, vmax=20.0)
+    # plot this graph in 3D by fitting a 3D polynomial function to the data
+    # Then plot the 3D polynomial function as a surface
+
+    # Create a meshgrid for the depth map
+    x_start = df['longitude'].min()
+    y_start = df['latitude'].min()
+    x_end = df['longitude'].max()
+    y_end = df['latitude'].max()
+    x = numpy.linspace(x_start, x_end, depth_map.shape[1])
+    y = numpy.linspace(y_start, y_end, depth_map.shape[0])
+    X, Y = numpy.meshgrid(x, y)
+
+    print("Plotting...")
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, depth_map, linewidth=0) #cmap='viridis', linewidth=0)
     plt.show()
 
-    '''
-    #Normalize the depth map to 0-255
-    depth_map = numpy.interp(depth_map, (depth_map.min(), depth_map.max()), (0, 255))
-
-    plt.imshow(depth_map, cmap='gray')
-    plt.show()
-    '''
 if __name__ == '__main__':
-    render_depth_map(generate_depth_map(df, 0.001))
+    render_depth_map(generate_depth_map(df, 0.05))
--- dbProject/python_version/main.py
+++ dbProject/python_version/main.py
@@ -4,6 +4,7 @@ import sqlite3
 import matplotlib
 from mpl_toolkits.basemap import Basemap
 import tkinter as tk
+import analysis as an
 
 matplotlib.use('TkAgg')
 
@@ -26,47 +27,35 @@ query = (
 
 df = pd.read_sql_query(query, conn)
 
-#Close the connection
-conn.close()
-
-#Example anomaly data
-anomalies = pd.DataFrame({
-    'latitude': [
-        34.0522,
-        34.0522,
-        34.0522,
-    ],
-    'longitude': [
-        -118.2437,
-        -118.2437,
-        -118.2437,
-    ]
-})
-
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
         #Create a window
         self.title("Switrs")
-        self.geometry("800x600")
+        self.geometry("1000x800")
 
         #Split the window into two panels
         self.left_panel = tk.Frame(self)
         self.right_panel = tk.Frame(self)
 
         #Create a figure in the left panel
-        fig = Figure(figsize=(6, 4), dpi=100)
+        self.fig = Figure(figsize=(6, 4), dpi=100)
 
         #Create a subplot to left panel
-        self.canvas = FigureCanvasTkAgg(fig, master=self.left_panel)
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.left_panel)
         NavigationToolbar2Tk(self.canvas, self.left_panel)
 
         #Create a subplot
-        ax = fig.add_subplot(111)
-
+        ax = self.fig.add_subplot(211)
+        ax2 = self.fig.add_subplot(212, projection='3d')
+        
+        #TODO: Make the 3D plot zoom match the 2D plot zoom
+        #Need to figure out how to get the current extent of the 2D plot in lat/long
+        #Then convert that to x/y and set the 3D plot extent to that
+        
         #Create the map
-        basemap = Basemap(
+        self.basemap = Basemap(
             projection='gall',
             llcrnrlon = -126,   # lower-left corner longitude
             llcrnrlat = 32,     # lower-left corner latitude
@@ -76,37 +65,196 @@ class App(tk.Tk):
         )
 
         #Draw the map
-        basemap.drawcoastlines()
-        basemap.drawcountries()
-        basemap.drawstates()
+        self.basemap.drawcoastlines()
+        self.basemap.drawcountries()
+        self.basemap.drawstates()
+        self.basemap.bluemarble()
 
         #Plot the points
-        x, y = basemap(df['longitude'].values, df['latitude'].values)
-        ax.plot(x, y, 'k.', markersize=1.5)
+        x, y = self.basemap(df['longitude'].values, df['latitude'].values)
+        #Black dots
+        ax.plot(x, y, '.', color='k', markersize=1.5)
+
+        #Plot the 3D points
+        an.plot_3d(an.generate_depth_map(df, 0.05), ax2)
+        ax2.set_xlabel('Longitude')
+        ax2.set_ylabel('Latitude')
+        ax2.set_zlabel('Number of Collisions')
+        
+        #Add a panel for the longitude query range feature
+        self.longitude_query_panel = tk.Frame(self.right_panel)
+        self.longitude_query_panel.pack(side=tk.TOP, fill=tk.X)
+        
+        #Add a text field for the longitude query range feature
+        self.longitude_query_label = tk.Label(self.longitude_query_panel, text="Longitude Range")
+        self.longitude_query_label.pack()
+        self.min_longitude = tk.Entry(self.longitude_query_panel)
+        self.min_longitude.pack(side=tk.LEFT)
+        self.max_longitude = tk.Entry(self.longitude_query_panel)
+        self.max_longitude.pack(side=tk.RIGHT)
+
+        #Add a panel for the latitude query range feature
+        self.latitude_query_panel = tk.Frame(self.right_panel)
+        self.latitude_query_panel.pack(side=tk.TOP, fill=tk.X)
+        
+        #Add a text field for the latitude query range feature
+        self.latitude_query_label = tk.Label(self.latitude_query_panel, text="Latitude Range")
+        self.latitude_query_label.pack()
+        self.min_latitude = tk.Entry(self.latitude_query_panel)
+        self.min_latitude.pack(side=tk.LEFT)
+        self.max_latitude = tk.Entry(self.latitude_query_panel)
+        self.max_latitude.pack(side=tk.RIGHT)
+
+        #Add a button to query the database
+        self.query_button = tk.Button(self.right_panel, text="Query", command=self.query)
+        self.query_button.pack()
 
         #Add a label for the listbox
-        self.label = tk.Label(self.right_panel, text="Anomalies")
+        self.label = tk.Label(self.right_panel, text="Clusters")
         self.label.pack()
 
         #Create a listbox in the right panel
         self.listbox = tk.Listbox(self.right_panel, width=50, height=30)
 
-        #Set the listbox to contain all the positions of the anomalies
-        for index, row in anomalies.iterrows():
-            self.listbox.insert(tk.END, "Collision at {}, {}".format(row['latitude'], row['longitude']))
-
         #Render the listbox
         self.listbox.pack()
+        self.listbox.bind('<Double-Button-1>', self.listbox_double_click)
+
+        #Create a panel for the add point text boxes
+        self.add_point_panel = tk.Frame(self.right_panel)
+        self.add_point_panel.pack()
+        self.add_point_label = tk.Label(self.add_point_panel, text="Enter longitude and latitude to add a point")
+        self.add_point_label.pack()
+
+        #Add two text fields to enter the coordinates
+        self.latitude = tk.Entry(self.add_point_panel)
+        self.latitude.pack(side=tk.LEFT)
+        self.longitude = tk.Entry(self.add_point_panel)
+        self.longitude.pack(side=tk.RIGHT)
+
+        #Create a button to add a point
+        self.add_point_button = tk.Button(self.right_panel, text="Add Point", command=self.add_point)
+        self.add_point_button.pack()
+
+        #Create a button to cluster the data in the current view
+        self.button = tk.Button(self.right_panel, text="Cluster", command=self.cluster)
+        self.button.pack()
 
         #Render the panels
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         self.left_panel.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
         self.right_panel.pack(side=tk.RIGHT, fill=tk.BOTH, expand=1)
 
-        print("Done")
-    
+    def cluster(self):
+        #Clear the listbox
+        self.listbox.delete(0, tk.END)
+
+        #Get the current extent of the map
+        xmin, xmax = self.fig.axes[0].get_xlim()
+        ymin, ymax = self.fig.axes[0].get_ylim()
+        
+        #Convert the extent to lat/long
+        xmin, ymin = self.basemap(xmin, ymin, inverse=True)
+        xmax, ymax = self.basemap(xmax, ymax, inverse=True)
+
+        #Get the current view of the map
+        view = df[(df['latitude'] >= ymin) & (df['latitude'] <= ymax) & (df['longitude'] >= xmin) & (df['longitude'] <= xmax)]
+
+        #Cluster the data
+        centroids, clusters = an.cluster(view)
+        
+        if (centroids is None or clusters is None):
+            return
+        
+        #Add the centroids to the listbox
+        for centroid in centroids:
+            self.listbox.insert(tk.END, "Collision at {}, {}".format(round(centroid[0], 4), round(centroid[1], 4)))
+
+        #self.fig.axes[0].clear()
+        #Clusters format (longitude, latitude, cluster)
+        #Plot the clusters with a different color for each cluster and the centroid of the cluster
+        # A list of random colors with length equal to the number of clusters
+        colors = [matplotlib.colors.rgb2hex(numpy.random.rand(3)) for i in range(len(centroids))]
+        for index, row in clusters.iterrows():
+            x, y = self.basemap(row['longitude'], row['latitude'])
+            self.fig.axes[0].plot(x, y, '.', color=colors[int(row['cluster'])], markersize=1.5)
+
+        for cluster in centroids:
+            x, y = self.basemap(centroid[0], centroid[1])
+            self.fig.axes[0].plot(x, y, 'r*', markersize=100)
+
+        self.canvas.draw()
+
+    def listbox_double_click(self, event):
+        # Zoom to the selected cluster
+        centroid = self.listbox.get(self.listbox.curselection())
+        centroid = centroid.split("at ")[1]
+        centroid = centroid.split(", ")
+        centroid = [float(centroid[0]), float(centroid[1])]
+        # Set extent of `self.fig.axes[0]` to the centroid with a 0.1 degree buffer
+        print(centroid)
+        centroid = self.basemap(centroid[0], centroid[1])
+        print(centroid)
+        self.fig.axes[0].set_xlim(centroid[0] - 0.1, centroid[0] + 0.1)
+        self.fig.axes[0].set_ylim(centroid[1] - 0.1, centroid[1] + 0.1)
+        self.canvas.draw()
+
+    def add_point(self):
+        #Read the latitude and longitude from the text boxes
+        latitude = float(self.latitude.get())
+        longitude = float(self.longitude.get())
+        #Ensure the latitude and longitude are valid
+        #TODO: Add toast message
+        if (latitude < -90 or latitude > 90 or longitude < -180 or longitude > 180):
+            return
+        #Add the point to the dataframe
+        df.loc[len(df)] = [latitude, longitude]
+        #Plot the point on the map
+        x, y = self.basemap(longitude, latitude)
+        self.fig.axes[0].plot(x, y, 'k.', markersize=1.5)
+        #Query insert the point into the database
+        conn.cursor.execute("INSERT INTO collisions (latitude, longitude) VALUES (%s, %s)", (latitude, longitude))
+        conn.conn.commit()
+        #Redraw the map
+        self.canvas.draw()
+
+    def query(self):
+        #Get min and max latitude and longitude from the text boxes
+        min_latitude = float(self.min_latitude.get())
+        max_latitude = float(self.max_latitude.get())
+        min_longitude = float(self.min_longitude.get())
+        max_longitude = float(self.max_longitude.get())
+        #Ensure the min and max latitude and longitude are valid
+        if (min_latitude < -90 or min_latitude > 90 or max_latitude < -90 or max_latitude > 90 or min_longitude < -180 or min_longitude > 180 or max_longitude < -180 or max_longitude > 180):
+            return
+
+        #Get the data from the df
+        data = df[(df['latitude'] >= min_latitude) & (df['latitude'] <= max_latitude) & (df['longitude'] >= min_longitude) & (df['longitude'] <= max_longitude)]
+        
+        #Redraw the map
+        self.fig.axes[0].clear()
+        self.basemap.drawcoastlines()
+        self.basemap.drawcountries()
+        self.basemap.drawstates()
+        self.basemap.bluemarble()
+
+        #Plot the data on the map
+        x, y = self.basemap(data['longitude'].values, data['latitude'].values)
+        self.fig.axes[0].plot(x, y, 'k.', markersize=1.5)
+
+        self.fig.axes[1].clear()
+        an.plot_3d(an.generate_depth_map(df, 0.05), self.fig.axes[1])
+        self.fig.axes[1].set_xlabel('Longitude')
+        self.fig.axes[1].set_ylabel('Latitude')
+        self.fig.axes[1].set_zlabel('Number of Collisions')
+
+        self.canvas.draw()
+
+
+
 if __name__ == '__main__':
     app = App()
     app.mainloop()
+    conn.close()
 
 
